864689e883b9ad1687fada368bbfd9dd2d566f58
terminatorDX <jain.cj.chetan@gmail.com>
2020-03-20 11:29:41 +0530

    created key.js

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientId:
+        "895459990820-epdtj7uinmbl891otrarucl3kmkmrbb9.apps.googleusercontent.com",
+    googleClientSecret: "902jka5CBl1RqfP1odmhXGHe",
+    googleApiKey: "AIzaSyAO9WsToHQ5d4PTIapx3sYiM8FjmlWT8e4"
+};
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express"),
+    passport = require("passport"),
+    googleStrategy = require("passport-google-oauth20").Strategy,
     PORT = process.env.PORT || 5000,
     app = express();
 app.get("/", (req, res) => {
     res.send({ hi: "there" });
 });
+
+passport.use(new googleStrategy());
 app.listen(PORT, () => {
     console.log("app is listening on port 5000");
 });
+
