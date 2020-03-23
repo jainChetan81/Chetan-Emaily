083e5e84b271bed09a24f17b9e0dbf3c81bc6e17
terminatorDX <jain.cj.chetan@gmail.com>
2020-03-23 20:48:41 +0530

    changed the status of keys

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,5 @@
-module.exports = {
-    googleClientId:
-        "895459990820-epdtj7uinmbl891otrarucl3kmkmrbb9.apps.googleusercontent.com",
-    googleClientSecret: "902jka5CBl1RqfP1odmhXGHe",
-    googleApiKey: "AIzaSyAO9WsToHQ5d4PTIapx3sYiM8FjmlWT8e4",
-    mongoURI: "mongodb://chetan:abc123abc@ds123372.mlab.com:23372/cjemaily-dev",
-    cookie: "coolboy"
-};
+if (process.env.NODE_ENV === "production") {
+    module.exports = require("./prod");
+} else {
+    module.exports = require("./dev");
+}
