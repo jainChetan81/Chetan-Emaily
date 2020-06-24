fa26b94c553c79e7eea8228d2a0e667945d89756
Chetan Jain <JAIN.CJ.CHETAN@gmail.com>
2020-06-24 16:26:11 +0530

    chnged some routes for heroku deployment

--- index.js
+++ index.js
@@ -19,6 +19,18 @@ app.use(passport.initialize());
 app.use(passport.session());
 authRoutes(app);
 billingRoutes(app); //or  authRouts = require("./routes/authRoutes")(app)
+if (process.env.NODE_ENV === "production") {
+    //express will serve up production assest
+    //like  our main.js or main.css files
+    app.use(express.static("client/build"));
+
+    //express will serve up index.js file if it doesn't recognizes the route
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
+
 app.listen(PORT, () => {
     console.log("app is listening on port 5000");
 });
