9f6502227d92ad3c2d85557cc28b5673df4ce6d4
unknown <jain.cj.chetan@gmail.com>
2020-12-18 17:59:26 +0530

    changing a afew things

--- client/src/components/Surveys/SurveyList.js
+++ client/src/components/Surveys/SurveyList.js
@@ -9,58 +9,67 @@ class SurveyList extends Component {
     }
 
     renderSurveys() {
-        const { feedbacks } = this.props;
-        return this.props.surveys.reverse().map((survey, index) => {
-            return (
-                <div className="card darken-1" key={survey._id}>
-                    <div className="card-content">
-                        <span className="card-title">{survey.title}</span>
-                        <p>{survey.body}</p>
-                        <p className="right">
-                            Sent On:
-                            {new Date(survey.dateSent).toLocaleDateString()}
-                        </p>
-                    </div>
-                    <div className="card-tabs">
-                        <ul className="collection">
-                            <li className="collection-item ">
-                                <div className="row">
-                                    <div className="col s10">
-                                        Q1:{" "}
-                                        {feedbacks[index]?.question1.question}
-                                    </div>
-                                    <div className="col s1">
-                                        Y: {feedbacks[index]?.question1.Yes}
-                                    </div>
-                                    <div className="col s1">
-                                        N: {feedbacks[index]?.question1.No}
-                                    </div>
-                                </div>
-                            </li>
-                            {feedbacks[index]?.Questions.map((ques, index) => {
-                                return (
-                                    <li
-                                        className="collection-item"
-                                        key={ques._id}>
-                                        <div className="row">
-                                            <div className="col s10">
-                                                Q{index + 1}: {ques.question}
-                                            </div>
-                                            <div className="col s1">
-                                                N: {ques.Yes}
-                                            </div>
-                                            <div className="col s1">
-                                                Y: {ques.No}
-                                            </div>
+        const { feedbacks, surveys } = this.props;
+        if (surveys.length > 0) {
+            return surveys.reverse().map((survey, index) => {
+                return (
+                    <div className="card darken-1" key={survey._id}>
+                        <div className="card-content">
+                            <span className="card-title">{survey.title}</span>
+                            <p>{survey.body}</p>
+                            <p className="right">
+                                Sent On:
+                                {new Date(survey.dateSent).toLocaleDateString()}
+                            </p>
+                        </div>
+                        <div className="card-tabs">
+                            <ul className="collection">
+                                <li className="collection-item ">
+                                    <div className="row">
+                                        <div className="col s10">
+                                            Q1:{" "}
+                                            {
+                                                feedbacks[index]?.question1
+                                                    .question
+                                            }
                                         </div>
-                                    </li>
-                                );
-                            })}
-                        </ul>
+                                        <div className="col s1">
+                                            Y: {feedbacks[index]?.question1.Yes}
+                                        </div>
+                                        <div className="col s1">
+                                            N: {feedbacks[index]?.question1.No}
+                                        </div>
+                                    </div>
+                                </li>
+                                {feedbacks[index]?.Questions.map(
+                                    (ques, index) => {
+                                        return (
+                                            <li
+                                                className="collection-item"
+                                                key={ques._id}>
+                                                <div className="row">
+                                                    <div className="col s10">
+                                                        Q{index + 1}:{" "}
+                                                        {ques.question}
+                                                    </div>
+                                                    <div className="col s1">
+                                                        N: {ques.Yes}
+                                                    </div>
+                                                    <div className="col s1">
+                                                        Y: {ques.No}
+                                                    </div>
+                                                </div>
+                                            </li>
+                                        );
+                                    }
+                                )}
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            );
-        });
+                );
+            });
+        }
+        if (surveys.length <= 0) return <h1>Nothng to show here</h1>;
     }
 
     render() {
